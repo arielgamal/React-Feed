8e62288d814052152f65ce5301c0f67068ca73b1
Ariel Gamal <ariel@gamal.com.br>
2022-11-10 13:51:21 -0300

    Iniciando o componente Sidebar e estilização CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,6 @@ import styles from './Header.module.css'
 
 import webLogo from '../assets/logo.png'
 
-console.log(webLogo)
-
 export function Header() {
   return (
     <header className={styles.header}>
